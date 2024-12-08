2b9d1fe5d0513cbcce69820c6d792782406eaa5a
Shane Xu <xusheng0711@gmail.com>
2024-12-08 23:45:10 +0800

    set default size

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -47,7 +47,7 @@ static void my_application_activate(GApplication* application) {
     gtk_window_set_title(window, "youdao_dict_for_linux");
   }
 
-  gtk_window_set_default_size(window, 1280, 720);
+  gtk_window_set_default_size(window, 640, 640);
   gtk_widget_show(GTK_WIDGET(window));
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
